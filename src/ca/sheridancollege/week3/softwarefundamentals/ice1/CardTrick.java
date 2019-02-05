e2fcd699ca4ce23f34311c7d926cde020ac92f62
Sudikshya-Bisoyi-Sheridan <bisoyi@sheridancollege.ca>
2019-02-05 11:49:31 -0500

    added name

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//sudikshya bisoyi 991451770
+
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
