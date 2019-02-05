ba66dd822f90b0a9d8bcbed6cb05f1e0b8103eda
Amika-Ota-Sheridan <otaa@sheridancollege.ca>
2019-02-05 11:50:17 -0500

    added array and random method

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,12 +20,21 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue((int) (Math.random()*((3-0)+1)));
             //c.setValue(insert call to random number generator here)
+            c.setSuit(Card.SUITS[c.setValue]);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
+        String array[] = new String[];
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        Scanner myInput = new Scanner(System.in);
+        
+        System.out.println("Enter a card value and suit");
+        array [0] = myInput.nextLine();
+        
+        
+// and search magicHand here
         //Then report the result here
     }
     
