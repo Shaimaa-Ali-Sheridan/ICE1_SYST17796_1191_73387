0d9713ab90a369c0394c3e29319114874e89cef5
Gps <Gps@LAPTOP-T9NBEFNA>
2019-02-05 13:51:21 -0500

    added code to take user input, compare and display result

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Gurpartap Gill 991529145
  */
 public class CardTrick {
     
