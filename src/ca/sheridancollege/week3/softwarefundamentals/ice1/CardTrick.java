fcf0b112f494909931aaf6198222867b765b6508
pasanghe <pasanghe@sheridancollege.ca>
2019-02-05 11:37:04 -0500

    added header and finished picking card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,12 +4,13 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @author SangHeon, Park
+ * @date February 5, 2019
  */
 public class CardTrick {
     
@@ -20,10 +21,9 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(Math.floor(Math.random() * 13) + 1)
+            c.setSuit(Card.SUITS[Math.floor(Math.random() * 3) + 1])
         }
-        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
