edf8953ff3dbf068b57bacea6e9063d7e074d257
pasanghe <pasanghe@sheridancollege.ca>
2019-02-05 11:45:13 -0500

    asking user's input for value and suite using JOptionPane

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import javax.swing.JOptionPane;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,6 +17,8 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
+        int userValue = 0;
+        String userSuite = "";
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
@@ -25,6 +28,8 @@ public class CardTrick {
             c.setSuit(Card.SUITS[Math.floor(Math.random() * 3) + 1])
         }
         //insert code to ask the user for Card value and suit, create their card
+        userValue = Integer.parseInt(JOptionPane.showInputDialog(null, "What is your card value?"));
+        userSuite = JOptionPane.showInputDialog(null, "What is your suite?");
         // and search magicHand here
         //Then report the result here
     }
