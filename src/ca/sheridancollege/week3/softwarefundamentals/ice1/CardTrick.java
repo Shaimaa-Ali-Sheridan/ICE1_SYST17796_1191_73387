254b2c704f972f44f26c6cb40e8eee697f06537b
razviz <razviz@sheridancollege.ca>
2019-02-05 11:37:40 -0500

    added a for loop and a modifier for name and date

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @author: Zain Razvi 
+ * @date: February 5, 2019
  */
 public class CardTrick {
     
@@ -20,12 +22,15 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(Math.random() * 13 + 1);
+            c.setSuit(Card.SUITS[Math.random() * 4]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner input = new Scanner(System.in);
+        
         // and search magicHand here
+        
         //Then report the result here
     }
     
