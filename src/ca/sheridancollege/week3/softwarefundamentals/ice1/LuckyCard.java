bfc4da2c5cab70797bee286f80af16ce9658929d
LucasZanlorensi <47357226+LucasZanlorensi@users.noreply.github.com>
2019-02-05 13:28:29 -0500

    Created LuckyCard.java

---
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/LuckyCard.java
@@ -0,0 +1,11 @@
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+public class LuckyCard extends Card{
+
+  LuckyCard(){
+    super();
+    super.setValue(2);
+    super.setSuit("Diamonds");
+  }
+
+}
