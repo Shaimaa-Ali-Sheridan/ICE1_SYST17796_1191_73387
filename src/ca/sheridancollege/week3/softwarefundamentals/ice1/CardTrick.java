e10bb25a6b0c46931dd23bc40781f4324591b177
gaunim <gaunim@sheridancollege.ca>
2019-02-05 11:49:35 -0500

    first commit - added random card suit/value algorithm, user input prompt & search function

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,22 +11,45 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+import java.util.*;
+
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
+        for (int i = 0; i< magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(1 + Math.random()* 13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(c.SUITS[(int)(Math.random() * 4)]);
+            magicHand[i] = c;            
         }
-        
+               
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        System.out.println("Enter a Card Value(1-13)");
+        int userInput1 = input.nextInt();
+        System.out.println("Enter a Suit(Hearts, Diamonds, Spades, Clubs");
+        input.nextLine();
+        String userInput2 = input.nextLine();
+        
+        Card userCard = new Card();
+        userCard.setValue(userInput1);
+        userCard.setSuit(userInput2);
+        
+         // and search magicHand here
+         
+        for(int i = 0; i < magicHand.length; i++){           
+            if(userCard.getValue() == magicHand[i].getValue() && userCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit())){
+                //Then report the result here
+               System.out.println("Card Match");
+            }        
+        }
+        System.out.println("No Match");       
     }
     
 }
