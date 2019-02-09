4d90e4c3216dfc63a9f636e8e820c3837ced29c0
pasanghe <pasanghe@sheridancollege.ca>
2019-02-09 15:32:33 -0500

    added package statement (erased by mistake)

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,15 +1,15 @@
 
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
 import javax.swing.JOptionPane;
-
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects and then
- * asks the user to pick a card and searches the array of cards for the match to
- * the user's card. To be used as starting code in ICE 1
- * 
+ * A class that fills a magic hand of 7 cards with random Card Objects
+ * and then asks the user to pick a card and searches the array of cards
+ * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  * @author SangHeon, Park
  * @date February 5, 2019
  */
+
 public class CardTrick {
 
 	public static void main(String[] args) {
