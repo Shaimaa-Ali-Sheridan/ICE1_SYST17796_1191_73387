1584e4431bc1688e4e279506e5ebe87fa3e41679
pasanghe <46933833+pasanghe@users.noreply.github.com>
2019-02-09 15:28:35 -0500

    fixed code for searching magicHand
    escape loop when user's card is same as one of 7 random cards
    + put a code block for printing 7 cards into method

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,53 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
 import javax.swing.JOptionPane;
+
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * A class that fills a magic hand of 7 cards with random Card Objects and then
+ * asks the user to pick a card and searches the array of cards for the match to
+ * the user's card. To be used as starting code in ICE 1
+ * 
  * @author dancye
  * @author SangHeon, Park
  * @date February 5, 2019
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        boolean match = false;
-        Card[] magicHand = new Card[7];
-	Card luckyCard = new Card();
-        
-        for (int i = 0; i < magicHand.length; i++) {
-	magicHand[i] = new Card();
-	magicHand[i].setValue((int) (Math.random() * 13) + 1);
-	magicHand[i].setSuit(Card.SUITS[(int) ((Math.random() * 3) + 1)]);
-        }
-        //insert code to ask the user for Card value and suit, create their card
-	luckyCard.setValue(Integer.parseInt(JOptionPane.showInputDialog(null, "What is your card value? 0 - 13")));
-	luckyCard.setSuit(JOptionPane.showInputDialog(null, "What is your suit? Diamonds | Spades | Hearts | Clubs"));
-        // and search magicHand here
-        for (int i = 0; i < magicHand.length; i++) {
-	if ((luckyCard.getValue() == magicHand[i].getValue()) && (luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))) {
-		match = true;
-	} else
-		match = false;
+
+	public static void main(String[] args) {
+		boolean match = false;
+		Card[] magicHand = new Card[7];
+		Card luckyCard = new Card();
+		for (int i = 0; i < magicHand.length; i++) {
+			magicHand[i] = new Card();
+			magicHand[i].setValue((int) (Math.random() * 13) + 1);
+			magicHand[i].setSuit(Card.SUITS[(int) ((Math.random() * 3) + 1)]);
+		}
+		// insert code to ask the user for Card value and suit, create their card
+		luckyCard.setValue(Integer.parseInt(JOptionPane.showInputDialog(null, "What is your card value? 0 - 13")));
+		luckyCard.setSuit(JOptionPane.showInputDialog(null, "What is your suit? Diamonds | Spades | Hearts | Clubs"));
+		// and search magicHand here
+		for (int i = 0; i < magicHand.length; i++) {
+			if ((luckyCard.getValue() == magicHand[i].getValue())
+					&& (luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))) {
+				match = true;
+				break;
+			} else
+				match = false;
+		}
+		System.out.println(match);
+		// Then report the result here
+		if (match == true)
+			System.out.println("Lucky!\nYour card was " + luckyCard.getValue() + " of " + luckyCard.getSuit());
+		else
+			System.out.println("Unlucky!\nYour card was " + luckyCard.getValue() + " of " + luckyCard.getSuit());
+		// print cards in magicHand
+		printDeck(magicHand);
 	}
-        //Then report the result here
-        if (match)
-        System.out.println(luckyCard.getValue() + " " + luckyCard.getSuit());
-        else
-	System.out.println("Unlucky!");
-        
-        //Displaying randomly generated cards and the card user picked
-//        for (int i = 0; i < magicHand.length; i++) {
-//            System.out.println(i);
-//            System.out.println(magicHand[i].getValue());
-//            System.out.println(magicHand[i].getSuit());
-//        }
-//        System.out.println(luckyCard.getValue() + "\n" + luckyCard.getSuit());
-    }    
+
+	static void printDeck(Card[] magicHand) {
+//		 Displaying randomly generated cards and the card user picked
+		for (int i = 0; i < magicHand.length; i++) {
+			System.out.println("Card #" + (i + 1));
+			System.out.println(magicHand[i].getValue() + " of " + magicHand[i].getSuit());
+		}
+	}
 }
