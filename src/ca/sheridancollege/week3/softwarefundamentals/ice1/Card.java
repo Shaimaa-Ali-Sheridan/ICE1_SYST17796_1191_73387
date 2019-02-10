ec3c21d5da06223421b9ba544ca6206e17305420
razviz <razviz@sheridancollege.ca>
2019-02-10 13:34:13 -0500

    Added a default constructor and two-args constructor, also modified the date, author (my name), and my student number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -12,7 +12,9 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
+ * @author Zain Razvi
+ * @Student# 991517290
+ * @date: February 10, 2019
  */
 public class Card {
 
@@ -20,6 +22,15 @@ public class Card {
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public Card()
+   {
+       
+   }
+   public Card(String suit, int value)
+   {
+       this.suit = suit;
+       this.value = value;
+   }
     /**
      * @return the suit
      */
