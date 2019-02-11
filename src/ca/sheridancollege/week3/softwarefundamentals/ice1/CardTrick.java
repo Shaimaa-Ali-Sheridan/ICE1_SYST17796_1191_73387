9b61ddc8ddbf6778e582e9c951a88a3df5a13e0e
abdikasa <46930306+abdikasa@users.noreply.github.com>
2019-02-11 16:25:03 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,29 +10,30 @@ import java.util.Random;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Said Abdikarim 991523345
  */
 public class CardTrick {
     
     public static void main(String[] args)
-    {
+    {   
+        /*Created a card object with the value of 4 and suit value of Diamonds*/
+        
         Card[] magicHand = new Card[7];
         Card card = new Card();
         card.setValue(4);
         card.setSuit("Diamonds");
+        
+        /*Sets random object by using setValue and setSuit method to generate a random card.*/
         Random rand = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
             c.setValue(1 + rand.nextInt(13));
             c.setSuit(Card.SUITS[1+ rand.nextInt(3)]);
-           magicHand[i] = c;
+            magicHand[i] = c;
         }
         
-         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result hereF
-        
+        //Below we have a boolean var with the name test, if test is false, the card matches the magichand, if not, it returns true.
         boolean test = true;
         for(int i = 0; i < magicHand.length; i++){
         if (card.equals(magicHand[i])){
@@ -40,17 +41,10 @@ public class CardTrick {
         }
         }
         
-        //Output:
+        //Outputs the result: If you win or if you lose, print a message to the screen.
         String status;
         status = (test == false) ? "You win!" : "You lose, haha!";
         System.out.println(status);
-        
-       
-        
-      
-        
-        
     }
-    
 }
 
