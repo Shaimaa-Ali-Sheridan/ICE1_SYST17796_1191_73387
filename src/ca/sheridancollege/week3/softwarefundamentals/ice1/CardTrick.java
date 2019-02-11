d5575f12dd4a1c61a3b4a42de181e5ae795f17e8
Parin Patel <mrcreater6@gmail.com>
2019-02-11 13:54:54 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -28,8 +28,6 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            int value;
-            String str;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             magicHand[i] = c;
@@ -39,11 +37,19 @@ public class CardTrick {
             System.out.println(c.getSuit());
         }
         
+        Card lucky_card = new Card();
+        lucky_card.setValue(5);
+        lucky_card.setSuit(Card.SUITS[1]);
+        
         boolean b = false;
-        System.out.println("Pick Card: ");
+        System.out.println("Pick up up your favourite card: ");
         card = scan.nextInt();
-        System.out.println("Suit: ");
+        System.out.println("Please Select The Suit : 1 = Clubs, 2 = Spades, 3 = Heart , 4 = Diamond : ");
         suit = scan.next();
+        
+        Card c = new Card();
+          c.setValue(card);
+          c.setSuit(Card.SUITS[suit]);
         
         for (int i=0; i<magicHand.length; i++)
         {
