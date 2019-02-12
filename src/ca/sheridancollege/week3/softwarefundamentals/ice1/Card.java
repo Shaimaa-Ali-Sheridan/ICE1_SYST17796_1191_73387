140043ba4061d0437e4d2c602cc6d6098ba04e52
layth <layth@192.168.0.15>
2019-02-11 20:44:35 -0500

    add name and student NO.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,4 +1,4 @@
-/*
+/*/*Modifier: LAYTH MAHFOODH /Student Number: 991523983
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
