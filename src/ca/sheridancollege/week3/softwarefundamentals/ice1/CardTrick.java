7e48bbb3af929ef28136e9e3f72136094c09fc03
layth <layth@192.168.0.15>
2019-02-11 22:19:55 -0500

    modifying setValue() and setSuit()

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -28,6 +28,19 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+                }
+System.out.print("Enter card value and suit ");
+Scanner input1 = new Scanner (System.in);
+String s = input.next();
+Scanner input2 = new Scanner (System.in);
+int i = input.Int.next();
+for (int i=0; i<magicHand.length; i++)
+       
+        //insert code to ask the user for Card value and suit, create their card
+        // and search magicHand here
+        //Then report the result here
+    }
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
